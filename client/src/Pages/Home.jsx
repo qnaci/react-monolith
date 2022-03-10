3002bfaf7098ef87e5f0762e80c068f195e82e54
icanq <icanq@hotmail.com>
2022-03-10 20:21:04 +0700

    feat: add new pages on the client

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,61 @@
+export default function Home() {
+  return (
+    <div className='content'>
+      <h1>Selamat datang</h1>
+      <p>Silahkan belanja</p>
+      <img src='https://via.placeholder.com/1200x200' alt='banner' />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+        tristique sapien ac dolor vehicula elementum. Vestibulum euismod tellus
+        nunc, sit amet imperdiet dui fringilla et. Proin hendrerit ultricies
+        risus nec porta. Phasellus sollicitudin tristique dolor ac accumsan.
+        Aenean vitae mauris ut purus semper elementum. Sed feugiat neque et ex
+        euismod tempus. Maecenas ut lacinia metus. Aenean id turpis vitae arcu
+        hendrerit pulvinar. Suspendisse potenti. Pellentesque faucibus odio
+        lacus. Vivamus dapibus, tellus ut mollis faucibus, nibh nisl blandit
+        erat, vitae scelerisque ex libero id est. Etiam vel laoreet nisi. Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse potenti.
+        Fusce placerat justo dui, quis faucibus sem lacinia vel. Donec quis
+        tellus non nibh sollicitudin mollis. Quisque non risus sed mi fermentum
+        ornare sed a leo. Aenean et pulvinar mauris. Nulla in lacus arcu.
+        Curabitur gravida pulvinar enim nec sagittis. Cras mauris enim, aliquam
+        id lectus non, tincidunt posuere magna. Curabitur eu accumsan sapien,
+        hendrerit dapibus turpis. Cras consequat, felis quis ultrices posuere,
+        sapien quam tincidunt mi, in luctus purus libero vitae sapien. Nunc
+        faucibus nisl nulla, sed sagittis urna tristique sed. Integer a sapien
+        nec nibh sollicitudin iaculis. Aliquam ullamcorper, nisi vel euismod
+        ullamcorper, neque est hendrerit augue, id pharetra odio eros et sem.
+        Phasellus quis tempor erat, imperdiet accumsan eros. Etiam mollis dui
+        enim, in sollicitudin metus elementum quis. Sed auctor nibh a hendrerit
+        faucibus. In massa ligula, iaculis tempus lacus at, posuere fermentum
+        nulla. Vestibulum in felis in sapien pulvinar ullamcorper bibendum sit
+        amet nisi. Etiam augue risus, maximus a porta et, ullamcorper in nibh.
+        Quisque hendrerit faucibus est, eget viverra odio finibus quis. Praesent
+        nec urna eget est finibus mollis. Quisque sit amet quam risus. Integer
+        vestibulum ante non lacus consectetur sodales. Proin et leo nec sem
+        sollicitudin aliquam et imperdiet nisi. Morbi elementum, mi et finibus
+        tincidunt, tellus felis elementum magna, sit amet imperdiet tellus
+        ligula facilisis tortor. Vestibulum elementum convallis accumsan. Nullam
+        ut enim ac justo cursus feugiat in eget mi. Maecenas tristique quis odio
+        et eleifend. Vestibulum ante ipsum primis in faucibus orci luctus et
+        ultrices posuere cubilia curae; Quisque at dolor massa. Donec urna nisi,
+        efficitur a dui varius, pulvinar ultricies neque. Quisque tempor sem ac
+        porttitor efficitur. Mauris risus mauris, semper vitae fermentum vel,
+        volutpat et purus. Proin ut tempus leo. Curabitur fringilla orci urna,
+        et condimentum nunc pharetra a. Nam in enim volutpat, accumsan massa
+        nec, congue lorem. Proin semper, nulla sed suscipit consequat, lorem
+        purus maximus nunc, vel condimentum ipsum risus in tortor. Aliquam sit
+        amet urna accumsan, eleifend elit at, tincidunt elit. Maecenas tempus
+        finibus quam eget fringilla. Aliquam bibendum efficitur justo ut
+        consequat. Vestibulum eu finibus risus. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Sed sagittis nulla quis magna molestie, nec
+        volutpat massa interdum. Donec eu malesuada sem. Proin ac odio eget
+        tellus suscipit interdum. Suspendisse potenti. Phasellus laoreet odio id
+        turpis commodo convallis. Aliquam vel nisi sed lorem interdum porta. Ut
+        fermentum nunc pretium, porta sapien sit amet, tempor neque. Praesent
+        fermentum laoreet rutrum. Vestibulum ac euismod tellus. Mauris a dui
+        blandit, imperdiet elit non, eleifend dui.
+      </p>
+    </div>
+  );
+}
---
+++ client/src/Pages/Tentang.jsx
@@ -0,0 +1,41 @@
+export default function Tentang() {
+  return (
+    <div className='content'>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+        volutpat purus augue, quis tristique felis fermentum sed. Praesent sed
+        sodales orci, venenatis mattis sem. Vestibulum aliquam vestibulum
+        lectus, eu bibendum magna tempor at. Mauris in est mollis, egestas justo
+        in, mollis sem. Sed fringilla mi sit amet massa faucibus pretium. Donec
+        erat quam, porta nec mattis vitae, molestie in sem. Interdum et
+        malesuada fames ac ante ipsum primis in faucibus. Mauris vitae tempor
+        leo. Vivamus luctus elit vel purus elementum tempor. Sed metus felis,
+        interdum sit amet mollis ut, efficitur ut neque. Integer gravida mattis
+        ex, quis scelerisque erat rutrum a. Pellentesque in iaculis sem. Nulla
+        eu lectus ut ligula luctus ornare. Nunc luctus vitae dolor sit amet
+        varius. Integer non metus ex. Nunc turpis purus, volutpat fringilla sem
+        quis, efficitur pretium arcu. Aenean faucibus orci arcu. Praesent sit
+        amet tristique augue, eget accumsan tortor. Nunc gravida ex vel laoreet
+        ultrices.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+        volutpat purus augue, quis tristique felis fermentum sed. Praesent sed
+        sodales orci, venenatis mattis sem. Vestibulum aliquam vestibulum
+        lectus, eu bibendum magna tempor at. Mauris in est mollis, egestas justo
+        in, mollis sem. Sed fringilla mi sit amet massa faucibus pretium. Donec
+        erat quam, porta nec mattis vitae, molestie in sem. Interdum et
+        malesuada fames ac ante ipsum primis in faucibus. Mauris vitae tempor
+        leo. Vivamus luctus elit vel purus elementum tempor. Sed metus felis,
+        interdum sit amet mollis ut, efficitur ut neque. Integer gravida mattis
+        ex, quis scelerisque erat rutrum a. Pellentesque in iaculis sem. Nulla
+        eu lectus ut ligula luctus ornare. Nunc luctus vitae dolor sit amet
+        varius. Integer non metus ex. Nunc turpis purus, volutpat fringilla sem
+        quis, efficitur pretium arcu. Aenean faucibus orci arcu. Praesent sit
+        amet tristique augue, eget accumsan tortor. Nunc gravida ex vel laoreet
+        ultrices.
+      </p>
+    </div>
+  );
+}
